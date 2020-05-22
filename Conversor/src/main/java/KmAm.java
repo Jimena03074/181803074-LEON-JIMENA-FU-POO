4e8b3a79d08f3a213b0e1e432f72648866a64d8b
jimena leon garcia <jimena.leon1174@uppuebla.edu.mx>
2020-05-22 18:28:42 -0500

    conversor

---
+++ Conversor/src/main/java/KmAm.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class KmAm {
+   
+    public static void main (String args[]){
+        
+        Scanner leer =  new Scanner(System.in);
+        double km=0;
+        double met=0;         
+
+        System.out.print("ingrese los kilometros que desea convertir a metros: ");
+        km = leer.nextDouble();
+        
+            met = km*1000;
+            System.out.println("tus metros son: " + met + "m" );
+    }   
+}
+
+    
+
